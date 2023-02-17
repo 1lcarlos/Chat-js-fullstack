bfec92d3345df29dd5fc7d32fba862db644ffb30
lcarlos <luiscardozoicg@gmail.com>
2023-02-17 15:32:30 -0500

    new structure of files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
+const path = require("path");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
@@ -9,16 +10,20 @@ const io = new Server(server);
     res.sendFile(__dirname + '/index.html');
   }); */
 
-  app.use(express.static("public"));
-  
-  io.on('connection', (socket) => {
-    console.log('a user connected');
-  });
-  
-  server.listen(3000, () => {
-    console.log('listening on *:3000');
-  });
+/* app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  }); */
+require("./sockets")(io);
 
+ app.use(express.static(path.join(__dirname, "public")));
+
+/*  io.on('connection', (socket) => {
+    console.log('a user connected');
+  }); */
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
 
 /* import { createServer } from "http";
 import express from "express";
