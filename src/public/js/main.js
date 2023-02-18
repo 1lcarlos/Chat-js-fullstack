a1efc877a6d6754f20cb017448d743af8899b439
lcarlos <luiscardozoicg@gmail.com>
2023-02-17 22:11:53 -0500

    conection socket message

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,1 +1,19 @@
-io();
+$(function(){
+    
+    const socket = io();
+    const mensajeForm = $('#mensaje-form')
+    const mensaje = $('#mensaje')
+    const chat = $('#chat')
+
+    mensajeForm.submit(e => {
+        e.preventDefault()
+        socket.emit('enviar mensaje',mensaje.val() )
+        mensaje.val('')
+    })
+
+    socket.on('nuevo mensaje', function(mensaje){
+        chat.append(mensaje + '<br/>')
+    })
+
+
+})
