497ba55ad6f2bde9a2ba0ba014e5d35e87d79052
lcarlos <luiscardozoicg@gmail.com>
2023-02-18 18:16:10 -0500

    socket to nickname

--- src/sockets.js
+++ src/sockets.js
@@ -1,6 +1,19 @@
 module.exports = function(io){
+
+    let nickNames = []; 
+
     io.on('connection', (socket) => {
         console.log('a user connected');
+        socket.on('nuevo usuario', (nick, callback) =>{
+          console.log(nick)
+          if (nickNames.indexOf(nick) !=-1) {
+            callback(false)
+          }else{
+            callback(true)
+            socket.nickname = nick;
+            nickNames.push(socket.nickname)
+          }
+        })
         socket.on('enviar mensaje', function(mensaje) {
           io.sockets.emit('nuevo mensaje', mensaje)
         })
