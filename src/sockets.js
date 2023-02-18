62d18de1cb25e6213079822ec6daa5d09c04b0cd
lcarlos <luiscardozoicg@gmail.com>
2023-02-17 22:10:47 -0500

    socket message connection

--- src/sockets.js
+++ src/sockets.js
@@ -1,6 +1,9 @@
 module.exports = function(io){
     io.on('connection', (socket) => {
         console.log('a user connected');
+        socket.on('enviar mensaje', function(mensaje) {
+          io.sockets.emit('nuevo mensaje', mensaje)
+        })
       });
 }
 
