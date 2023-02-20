78ecffe3a13870ad7bf2eafe1304d803da8836ff
lcarlos <luiscardozoicg@gmail.com>
2023-02-19 23:13:13 -0500

    format and socket to nicknames users

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,32 +1,60 @@
-$(function(){
-    
-    const socket = io();
-    const mensajeForm = $('#mensaje-form')
-    const mensaje = $('#mensaje')
-    const chat = $('#chat')
+$(function () {
+  const socket = io();
+  const mensajeForm = $("#mensaje-form");
+  const mensaje = $("#mensaje");
+  const chat = $("#chat");
 
-    const nickForm = $('#nickform')
-    const nickName = $('#nickname')
-    const error = $('#error')
+  const nickForm = $("#nickform");
+  const nickName = $("#nickname");
+  const error = $("#error");
 
-    const users = $('#usernames')
+  const users = $("#usernames");
 
-    nickForm.submit(e => {
-        e.preventDefault()
-        socket.emit('nuevo usuario',nickName.val(), nick => {
+  nickForm.submit((e) => {
+    e.preventDefault();
+    socket.emit("nuevo usuario", nickName.val(), (nick) => {
+      if (nick) {
+        $("#contenedor-login").hide();
+        $("#contenedor").show();
+        $("#contenedor").css("display", "flex");
+      } else {
+        error.html(`<div>
+                    el usuario ya existe
+                </div>`);
+      }
+    });
+    nickName.val("");
+  });
+  mensajeForm.submit((e) => {
+    e.preventDefault();
+    socket.emit("enviar mensaje", mensaje.val());
+    mensaje.val("");
+  });
 
-        } )
-        nickName.val('')
-    })
-    mensajeForm.submit(e => {
-        e.preventDefault()
-        socket.emit('enviar mensaje',mensaje.val() )
-        mensaje.val('')
-    })
-
-    socket.on('nuevo mensaje', function(mensaje){
-        chat.append(mensaje + '<br/>')
-    })
-
-
-})
+  socket.on("nuevo mensaje", function (mensaje) {
+    chat.append(mensaje + "<br/>");
+  });
+  socket.on("usernames", (datos) => {
+    let html = "";
+    datos.forEach((user) => {
+      html += `
+      <div
+    class="flex flex-row py-4 px-2 justify-center items-center border-b-2"
+  >
+    <div class="w-1/4">
+      <img
+        src="https://source.unsplash.com/_7LbC5J-jw4/600x600"
+        class="object-cover h-12 w-12 rounded-full"
+        alt=""
+      />
+    </div>
+    <div class="w-full">
+      <div class="text-lg font-semibold">${user}</div>
+      
+    </div>
+  </div>
+      `;
+    });
+    users.html(html);
+  });
+});
