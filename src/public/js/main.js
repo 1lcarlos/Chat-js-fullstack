94551ffa001554b8fd03311aef484fcd8220be8e
lcarlos <luiscardozoicg@gmail.com>
2023-02-20 23:07:44 -0500

    add unavatar.io by img user

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -43,7 +43,7 @@ $(function () {
   >
     <div class="w-1/4">
       <img
-        src="https://source.unsplash.com/_7LbC5J-jw4/600x600"
+        src="https://unavatar.io/${user}"
         class="object-cover h-12 w-12 rounded-full"
         alt=""
       />
