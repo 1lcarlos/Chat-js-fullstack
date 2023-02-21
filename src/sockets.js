7ee0f535e1c2706b5da2046ada824bd616d67f4c
lcarlos <luiscardozoicg@gmail.com>
2023-02-21 10:36:45 -0500

    add socket disconnect and update nick function

--- src/sockets.js
+++ src/sockets.js
@@ -1,24 +1,33 @@
-module.exports = function(io){
+module.exports = function (io) {
+  let nickNames = [];
 
-    let nickNames = []; 
-
-    io.on('connection', (socket) => {
-        console.log('a user connected');
-        socket.on('nuevo usuario', (nick, callback) =>{
-          console.log(nick)
-          if (nickNames.indexOf(nick) !=-1) {
-            callback(false)
-          }else{
-            callback(true)
-            socket.nickname = nick;
-            nickNames.push(socket.nickname)
-            io.sockets.emit('usernames', nickNames)
-          }
-        })
-        socket.on('enviar mensaje', function(mensaje) {
-          io.sockets.emit('nuevo mensaje', mensaje)
-        })
+  io.on("connection", (socket) => {
+    console.log("a user connected");
+    socket.on("nuevo usuario", (nick, callback) => {
+      
+      if (nickNames.indexOf(nick) != -1) {
+        callback(false);
+      } else {
+        callback(true);
+        socket.nickname = nick;
+        nickNames.push(socket.nickname);
+        actualizarNickNames();
+      }
+    });
+    socket.on("enviar mensaje", function (mensaje) {
+      io.sockets.emit("nuevo mensaje", {
+        data: mensaje,
+        nick: socket.nickname
       });
-}
+    });
 
-
+    socket.on("disconnect", nick => {
+      if (!socket.nickname) return;
+      nickNames.splice(nickNames.indexOf(socket.nickname), 1);
+      actualizarNickNames();
+    });
+    function actualizarNickNames() {
+      io.sockets.emit("usernames", nickNames);
+    }
+  });
+};
