438fae04568c6e458005622aee63b711fe5e2b75
lcarlos <luiscardozoicg@gmail.com>
2023-02-22 16:12:14 -0500

    change socket to message private

--- src/sockets.js
+++ src/sockets.js
@@ -1,33 +1,52 @@
 module.exports = function (io) {
-  let nickNames = [];
+  let usuarios = {};
 
   io.on("connection", (socket) => {
     console.log("a user connected");
     socket.on("nuevo usuario", (nick, callback) => {
-      
-      if (nickNames.indexOf(nick) != -1) {
+      if (nick in usuarios) {
         callback(false);
       } else {
         callback(true);
         socket.nickname = nick;
-        nickNames.push(socket.nickname);
+        usuarios[socket.nickname] = socket;
         actualizarNickNames();
       }
     });
-    socket.on("enviar mensaje", function (mensaje) {
-      io.sockets.emit("nuevo mensaje", {
-        data: mensaje,
-        nick: socket.nickname
-      });
+    socket.on("enviar mensaje",  (mensaje, callback) => {
+      let msg = mensaje.trim();
+      if (msg.substr(0, 3) === "/w") {
+        msg = msg.substr(3);
+        const index = msg.indexOf(" ");
+        if (index !== -1) {
+          let name = msg.substring(0, index);
+          let msg = msg.substring(index + 1);
+          if (name in usuarios) {
+            usuarios[name].emit("whisper", {
+              msg,
+              nick: socket.nickname,
+            });
+          } else {
+            callback("Error valide el usuario antes de continuar");
+          }
+        } else {
+          callback("Error por favor escribe tu mensaje");
+        }
+      } else {
+        io.sockets.emit("nuevo mensaje", {
+          msg: mensaje,
+          nick: socket.nickname,
+        });
+      }
     });
 
-    socket.on("disconnect", nick => {
+    socket.on("disconnect", (nick) => {
       if (!socket.nickname) return;
-      nickNames.splice(nickNames.indexOf(socket.nickname), 1);
+      delete usuarios[socket.nickname];
       actualizarNickNames();
     });
     function actualizarNickNames() {
-      io.sockets.emit("usernames", nickNames);
+      io.sockets.emit("usernames", Object.keys(usuarios));
     }
   });
 };
